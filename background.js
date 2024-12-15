d917df63c169ae55740084d31ed24b5d813f90d7
kavyachan0-0 <kavyachan5@gmail.com>
2024-12-15 09:41:04 +0530

    added local storage

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.browserAction.onClicked.addListener(() => {
-    chrome.windows.create({
-      url: "index.html", // Your main file
-      type: "popup",
-      width: 5000,
-      height: 1000
-    });
-  });
-  
